7e3bacadf70f2026238d486beeb27cd5a2f7c0dd
이승열 <develten@naver.com>
2024-02-26 12:45:52 +0900

    grunt!

---
+++ Gruntfile.js
@@ -0,0 +1,57 @@
+module.exports = function (grunt) {
+  const config = require('./.screeps.json');
+  const branch = grunt.option('branch') || config.branch;
+  const email = grunt.option('email') || config.email;
+  const ptr = grunt.option('ptr') ? true : config.ptr;
+  const token = grunt.option('token') ? true : config.token;
+  const password = grunt.option('password') ? true : config.password;
+
+  grunt.loadNpmTasks('grunt-screeps');
+
+  grunt.initConfig({
+    screeps: {
+      options: {
+        email: email,
+        token: token,
+        branch: branch,
+        password: password,
+        ptr: ptr,
+        // server: 'season'
+      },
+      dist: {
+        src: ['src/*.js'],
+      },
+
+      mmo: {
+        options: {
+          server: 'persistent',
+        },
+        src: ['src/*.js'],
+      },
+
+      season: {
+        options: {
+          server: 'season',
+        },
+        src: ['src/*.js'],
+      },
+
+      private: {
+        options: {
+          token: undefined,
+          server: {
+            host: '127.0.0.1',
+            port: 21025,
+            http: true,
+          },
+        },
+        src: ['src/*.js'],
+      },
+    },
+  });
+
+  grunt.registerTask('default', ['screeps:mmo']);
+  grunt.registerTask('season', ['screeps:season']);
+  grunt.registerTask('private', ['screeps:private']);
+  grunt.registerTask('all', ['screeps:mmo', 'screeps:season', 'screeps:private']);
+};
